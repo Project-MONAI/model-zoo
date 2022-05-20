13a20d5ea092b6e67cb07d8fe2b7c017c9b41f99
Yiheng Wang <vennw@nvidia.com>
2022-05-20 18:26:57 +0800

    update workflow

--- .github/update_model_info.py
+++ .github/update_model_info.py
@@ -14,8 +14,8 @@ import hashlib
 import json
 import os
 import shutil
+import subprocess
 import tempfile
-import zipfile
 
 from monai.utils import look_up_option
 
@@ -32,11 +32,6 @@ def get_json_dict(json_dict_path: str):
     return json_dict
 
 
-def save_model_info(model_info_dict, model_info_path: str):
-    with open(model_info_path, "w") as f:
-        json.dump(model_info_dict, f)
-
-
 def get_hash_func(hash_type: str = "sha1"):
     actual_hash_func = look_up_option(hash_type.lower(), SUPPORTED_HASH_TYPES)
     return actual_hash_func()
@@ -48,7 +43,7 @@ def update_model_info(models_path: str, model_info_file: str = "model_info.json"
     hash_func = get_hash_func()
     # make a temp dir to store compressed bundles
     temp_dir = tempfile.mkdtemp()
-
+    ischanged = False
     task_list = get_sub_folders(models_path)
     for task in task_list:
         if task not in model_info.keys():
@@ -59,43 +54,58 @@ def update_model_info(models_path: str, model_info_file: str = "model_info.json"
             if bundle not in model_info[task].keys():
                 model_info[task][bundle] = {"version": "", "checksum": "", "source": ""}
             bundle_path = os.path.join(task_path, bundle)
-            # need to add a ci test to ensure that bundles must contain `configs/metadata.json`
-            # with changelog inside it.
             bundle_metadata_path = os.path.join(bundle_path, "configs/metadata.json")
             if os.path.exists(bundle_metadata_path):
                 metadata = get_json_dict(bundle_metadata_path)
                 # get version number from metadata
-                latest_version = list(metadata["changelog"].keys())[0]
+                latest_version = metadata["version"]
                 # compress bundle
-                bundle_zip_name = f"{bundle}_v{latest_version}.zip"
+                bundle_zip_name = f"{bundle}_v{latest_version}.tar"
+                compress_bundle(root_path=task_path, bundle_name=bundle, bundle_zip_name=bundle_zip_name)
                 dst_path = os.path.join(temp_dir, bundle_zip_name)
-                compress_bundle(source_path=bundle_path, dst_path=dst_path)
+                shutil.move(os.path.join(task_path, bundle_zip_name), dst_path)
                 # get actual checksum
                 actual_checksum = get_checksum(dst_path=dst_path, hash_func=hash_func)
 
                 # check consistency
-                if model_info[task][bundle]["version"] != latest_version:
-                    model_info[task][bundle]["version"] = latest_version
-                    changed_flag = True
                 if model_info[task][bundle]["checksum"] != actual_checksum:
+                    # upload new zip
+                    new_source = upload_bundle(dst_path, bundle_zip_name)
+                    # update model_info
                     model_info[task][bundle]["checksum"] = actual_checksum
-                    changed_flag = True
-                # if version or checksum is changed, upload the new bundle
-                if changed_flag is True:
-                    new_source = upload_bundle(dst_path)
+                    model_info[task][bundle]["version"] = latest_version
                     model_info[task][bundle]["source"] = new_source
+                    ischanged = True
+
+    if ischanged is True:
+        push_model_info(model_info, model_info_path)
 
     shutil.rmtree(temp_dir)
-    print(model_info)
 
 
-def compress_bundle(source_path: str, dst_path: str):
+def push_model_info(model_info_dict, model_info_path: str):
+    with open(model_info_path, "w") as f:
+        json.dump(model_info_dict, f)
 
-    ziph = zipfile.ZipFile(dst_path, "w", zipfile.ZIP_DEFLATED)
-    for root_dir, _, filenames in os.walk(source_path):
-        ziph.write(root_dir)
-        for file in filenames:
-            ziph.write(os.path.join(root_dir, file))
+    commit_message = "git commit -m 'auto update model_info'"
+    merged_pr_num = 10
+    # merged_pr_num = os.environ["PR_NUMBER"]
+    branch_name = f"{merged_pr_num}-auto-update-model-info"
+    create_push_cmd = f"git checkout -b {branch_name}; git push --set-upstream origin {branch_name}"
+
+    gh_pr_cmd = f"gh pr create --title '{merged_pr_num} Auto Update Model Info' --base dev"
+
+    full_cmd = f"git add {model_info_path}; {commit_message}; {create_push_cmd}; {gh_pr_cmd}"
+    print(full_cmd)
+    # subprocess.run(full_cmd, shell=True)
+
+
+def compress_bundle(root_path: str, bundle_name: str, bundle_zip_name: str):
+
+    deterministic_cmd = (
+        "--sort=name --owner=0 --group=0 --mode='go-rwx,u-rw' --numeric-owner --mtime='UTC 2022-06-01' -c"
+    )
+    subprocess.call(f"tar {deterministic_cmd} {bundle_name} | gzip -n > {bundle_zip_name}", shell=True, cwd=root_path)
 
 
 def get_checksum(dst_path: str, hash_func):
@@ -105,13 +115,17 @@ def get_checksum(dst_path: str, hash_func):
     return hash_func.hexdigest()
 
 
-def upload_bundle(bundle_zip_file_path: str):
-    """
-    to be implemented:
-    1) upload to ngc/github release
-    2) return new link
-    """
-    source = ""
+def upload_bundle(
+    bundle_zip_file_path: str,
+    bundle_zip_filename: str,
+    release_tag: str = "hosting_storage_v1",
+    repo_name: str = "Project-MONAI/model-zoo",
+):
+
+    upload_command = f"gh release upload {release_tag} {bundle_zip_file_path} -R {repo_name}"
+    # subprocess.run(upload_command, shell=True)
+    source = f"https://github.com/{repo_name}/releases/download/{release_tag}/{bundle_zip_filename}"
+
     return source
 
 
