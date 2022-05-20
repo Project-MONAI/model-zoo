426ce16484344b629299a48ff2e0f877cb5f05c7
Yiheng Wang <vennw@nvidia.com>
2022-05-20 14:40:11 +0800

    test py env

--- .github/update_model_info.py
+++ .github/update_model_info.py
@@ -119,13 +119,13 @@ def upload_bundle(
     release_tag: str = "hosting_storage_v1",
     repo_name: str = "Project-MONAI/model-zoo",
 ):
-
+    if os.getenv("GITHUB_TOKEN"):
+        print("GITHUB_TOKEN achieved.")
     upload_command = (
         f"gh release upload {release_tag} {bundle_zip_file_path} -R {repo_name}"
     )
     print("Upload bundle: ", bundle_zip_filename)
     subprocess.call(upload_command, shell=True)
-    print("Upload successful.")
     source = f"https://github.com/{repo_name}/releases/download/{release_tag}/{bundle_zip_filename}"
 
     return source
