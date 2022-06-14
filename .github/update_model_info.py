7093e117443db252d136a987d544fccfecedabdb
Yiheng Wang <vennw@nvidia.com>
2022-06-14 15:27:30 +0800

    remove duplicate try raise

--- .github/update_model_info.py
+++ .github/update_model_info.py
@@ -127,10 +127,7 @@ def main():
             else:
                 raise AssertionError(f"update bundle: {bundle} failed. {msg}")
     # push a new branch that contains the updated model_info.json
-    try:
-        push_new_model_info_branch(model_info_path=os.path.join(models_path, model_info_file))
-    except Exception as e:
-        raise AssertionError(f"push new branch error: {e}")
+    push_new_model_info_branch(model_info_path=os.path.join(models_path, model_info_file))
 
 
 if __name__ == "__main__":
