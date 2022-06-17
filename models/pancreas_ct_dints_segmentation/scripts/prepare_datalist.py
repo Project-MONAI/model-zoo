bd7ca604fe0c83a3b06d3bd289325a279a8283c6
dongy <dongy@nvidia.com>
2022-06-17 06:59:19 -0700

    update repo
    Signed-off-by: dongy <dongy@nvidia.com>

--- models/pancreas_ct_dints_segmentation/scripts/prepare_datalist.py
+++ models/pancreas_ct_dints_segmentation/scripts/prepare_datalist.py
@@ -52,10 +52,7 @@ if __name__ == "__main__":
         help="root path of MSD Task07_Pancreas dataset.",
     )
     parser.add_argument(
-        "--output",
-        type=str,
-        default="dataset_0.json",
-        help="relative path of output datalist json file.",
+        "--output", type=str, default="dataset_0.json", help="relative path of output datalist json file."
     )
     args = parser.parse_args()
 
