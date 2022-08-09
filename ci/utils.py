657cd3e2084d36160d130c38f2a73aa6dc7bf0b8
Yiheng Wang <vennw@nvidia.com>
2022-08-09 11:40:58 +0800

    fix ci

--- ci/utils.py
+++ ci/utils.py
@@ -85,8 +85,10 @@ def save_model_info(model_info_dict, model_info_path: str):
 
 def push_new_model_info_branch(model_info_path: str):
     merged_pr_num = os.environ["PR_NUMBER"]
-    email = os.environ["email"]
-    username = os.environ["username"]
+    # email = os.environ["email"]
+    # username = os.environ["username"]
+    email = "vennw@nvidia.com"
+    username = "yiheng-wang-nv"
 
     branch_name = f"{merged_pr_num}-auto-update-model-info"
     create_push_cmd = f"git checkout -b {branch_name}; git push --set-upstream origin {branch_name}"
