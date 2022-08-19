a057a1ebecb93f4723d47f23fb7186e4db3bbeeb
tangy5 <58751975+tangy5@users.noreply.github.com>
2022-08-19 00:56:05 -0500

    64-wholeBrain-LargeUNEST-segmentation (#84)
    * test commit 3
    
    * yt unest renalseg bundle commit v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * yt unest renalseg bundle commit v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * yt unest renalseg bundle commit v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * yt unest renalseg bundle commit v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * yt unest renalseg bundle commit v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * UNEST Renal Seg bundle
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * unest renal seg bundle v3
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * unest renal seg bundle v3
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * unest renal seg bundle v3
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * unest renal seg bundle v3
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * 0804
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * 0804
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * 0805
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * 808
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * wholeBrain 1st commit
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrain 1st commit
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrain 1st commit
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrainSeg bundle v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * wholeBrainSeg v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrainSeg bundle v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrainSeg bundle v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrainSeg bundle v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrainSeg bundle v2
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrainSeg bundle v3
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrainSeg bundle v3
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrainSeg bundle v4
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * wholeBrainSeg bundle v4
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * version fix
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * version fix
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * version fix
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Yiheng Wang <68361391+yiheng-wang-nv@users.noreply.github.com>

---
+++ models/wholeBrainSeg_Large_UNEST_segmentation/scripts/networks/nest/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from .utils import (
+    Conv3dSame,
+    DropPath,
+    Linear,
+    Mlp,
+    _assert,
+    conv3d_same,
+    create_conv3d,
+    create_pool3d,
+    get_padding,
+    get_same_padding,
+    pad_same,
+    to_ntuple,
+    trunc_normal_,
+)
