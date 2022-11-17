e1fb048368ce0c27a7af9c9630858c08b62a068d
Wenqi Li <831580+wyli@users.noreply.github.com>
2022-11-17 16:44:30 +0000

    208 adds cpu premerge tests github (#206)
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    Fixes #208 
    
    ### Description
    adds premerge github tests
    
    tested:
    https://github.com/Project-MONAI/model-zoo/actions/workflows/premerge-cpu.yml
    
    ### Please ensure all the checkboxes:
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Codeformat tests passed locally by running `./runtests.sh
    --codeformat`.
    - [x] In-line docstrings updated.
    - [ ] Update `version` and `changelog` in `metadata.json` if changing an
    existing bundle.
    - [ ] Please ensure the naming rules in config files meet our
    requirements (please refer to: `CONTRIBUTING.md`).
    - [ ] Ensure versions of packages such as `monai`, `pytorch` and `numpy`
    are correct in `metadata.json`.
    - [ ] Descriptions should be consistent with the content, such as
    `eval_metrics` of the provided weights and TorchScript modules.
    - [ ] Files larger than 25MB are excluded and replaced by providing
    download links in `large_file.yml`.
    - [ ] Avoid using path that contains personal information within config
    files (such as use `/home/your_name/` for `"bundle_root"`).
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- ci/run_premerge_cpu.sh
+++ ci/run_premerge_cpu.sh
@@ -41,11 +41,52 @@ remove_pipenv() {
     rm Pipfile Pipfile.lock
 }
 
+verify_bundle() {
+    echo 'Run verify bundle...'
+    init_pipenv requirements-dev.txt
+    head_ref=$(git rev-parse HEAD)
+    git fetch origin dev $head_ref
+    # achieve all changed files in 'models'
+    changes=$(git diff --name-only $head_ref origin/dev -- models)
+    if [ ! -z "$changes" ]
+    then
+        # get all changed bundles
+        bundle_list=$(pipenv run python $(pwd)/ci/get_changed_bundle.py --f "$changes")
+        if [ ! -z "$bundle_list" ]
+        then
+            pipenv run python $(pwd)/ci/prepare_schema.py --l "$bundle_list"
+            echo $bundle_list
+            for bundle in $bundle_list;
+            do
+                init_pipenv requirements-dev.txt
+                # get required libraries according to the bundle's metadata file
+                requirements=$(pipenv run python $(pwd)/ci/get_bundle_requirements.py --b "$bundle")
+                if [ ! -z "$requirements" ]; then
+                    echo "install required libraries for bundle: $bundle"
+                    pipenv install -r "$requirements"
+                fi
+                # verify bundle
+                pipenv run python $(pwd)/ci/verify_bundle.py -b "$bundle" -m "min"  # min tests on cpu
+                remove_pipenv
+            done
+        else
+            echo "this pull request does not change any bundles, skip verify."
+        fi
+    else
+        echo "this pull request does not change any files in 'models', skip verify."
+        remove_pipenv
+    fi
+}
+
 
 case $BUILD_TYPE in
 
     all)
         echo "Run all tests..."
+        ;;
+    changed)
+        echo "Run changed tests..."
+        verify_bundle
         ;;
     *)
         echo "ERROR: unknown parameter: $BUILD_TYPE"
