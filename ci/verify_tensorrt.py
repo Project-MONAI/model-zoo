398493cf5d624e1733c39d6b2b6a9b1964e58ca7
Yiheng Wang <vennw@nvidia.com>
2023-03-15 16:37:16 +0800

    modify verify function

--- ci/verify_tensorrt.py
+++ ci/verify_tensorrt.py
@@ -9,27 +9,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import argparse
-
-from utils import get_changed_bundle_list
+from bundle_custom_data import include_verify_tensorrt_list
 
 
-def verify_tensorrt(changed_dirs):
+def verify_tensorrt():
     """
     This function is used to verify if the checkpoint is able to export into TensorRT module.
     This function will be updated after the following PR is merged:
     https://github.com/Project-MONAI/MONAI/pull/5986/files
 
     """
-    root_path = "models"
-    bundle_list = get_changed_bundle_list(changed_dirs, tensorrt_only=True, root_path=root_path)
-    for bundle in bundle_list:
+    for bundle in include_verify_tensorrt_list:
         print(f"export bundle {bundle} weights into TensorRT module successfully.")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument("-f", "--f", type=str, help="changed files.")
-    args = parser.parse_args()
-    changed_dirs = args.f.splitlines()
-    verify_tensorrt(changed_dirs)
+    verify_tensorrt()
