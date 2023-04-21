c7d0eebcb11ca0a44cded5412a383dce2f2b6d19
binliunls <107988372+binliunls@users.noreply.github.com>
2023-04-21 18:26:16 +0800

    Add-onnx-workaround-benchmark-to-corresponding-bundles (#347)
    Fixes #346  .
    
    ### Description
    Add the ONNX-TensorRT conversion command to the `CONTRIBUTING.md`.
    Add the ONNX-TensorRT benchmark results to the bundles using this
    conversion.
    
    ### Status
    Work in progress
    
    ### Please ensure all the checkboxes:
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Codeformat tests passed locally by running `./runtests.sh
    --codeformat`.
    - [ ] In-line docstrings updated.
    - [ ] Update `version` and `changelog` in `metadata.json` if changing an
    existing bundle.
    - [ ] Please ensure the naming rules in config files meet our
    requirements (please refer to: `CONTRIBUTING.md`).
    - [ ] Ensure versions of packages such as `monai`, `pytorch` and `numpy`
    are correct in `metadata.json`.
    - [ ] Descriptions should be consistent with the content, such as
    `eval_metrics` of the provided weights and TorchScript modules.
    - [ ] Files larger than 25MB are excluded and replaced by providing
    download links in `large_file.yml`.
    - [ ] Avoid using path that contains personal information within config
    files (such as use `/home/your_name/` for `"bundle_root"`).
    
    ---------
    
    Signed-off-by: binliu <binliu@nvidia.com>
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>
    Co-authored-by: Yiheng Wang <68361391+yiheng-wang-nv@users.noreply.github.com>
    Co-authored-by: Yiheng Wang <vennw@nvidia.com>

--- ci/verify_tensorrt.py
+++ ci/verify_tensorrt.py
@@ -12,28 +12,33 @@
 import os
 
 import torch
-from bundle_custom_data import include_verify_tensorrt_list
+from bundle_custom_data import include_verify_onnx_tensorrt_list, include_verify_tensorrt_list
 from download_latest_bundle import download_latest_bundle
 from monai.bundle import trt_export
 from verify_bundle import _find_bundle_file
 
 
-def verify_tensorrt(bundle_path: str, net_id: str, config_file: str, precision: str):
+def verify_tensorrt(export_context):
     """
     This function is used to verify if the checkpoint is able to export into TensorRT model, and
     the exported model will be checked if it is able to be loaded successfully.
 
     """
+    bundle_path = export_context["bundle_path"]
+    precision = export_context["precision"]
+    config_file = export_context["config_file"]
     trt_model_path = os.path.join(bundle_path, f"models/model_trt_{precision}.ts")
     try:
         trt_export(
-            net_id=net_id,
+            net_id=export_context["net_id"],
             filepath=trt_model_path,
             ckpt_file=os.path.join(bundle_path, "models/model.pt"),
             meta_file=os.path.join(bundle_path, "configs/metadata.json"),
             config_file=os.path.join(bundle_path, config_file),
             precision=precision,
             bundle_root=bundle_path,
+            use_onnx=export_context["use_onnx"],
+            use_trace=export_context["use_trace"],
         )
     except Exception as e:
         print(f"'trt_export' failed with error: {e}")
@@ -45,28 +50,62 @@ def verify_tensorrt(bundle_path: str, net_id: str, config_file: str, precision: 
         raise
 
 
+def get_export_required_files(bundle: str, download_path: str, use_onnx: bool = False, use_trace: bool = False):
+    download_latest_bundle(bundle_name=bundle, models_path="models", download_path=download_path)
+    bundle_path = os.path.join(download_path, bundle)
+    net_id, inference_file_name = "network_def", _find_bundle_file(os.path.join(bundle_path, "configs"), "inference")
+    config_file = os.path.join("configs", inference_file_name)
+    export_context = {
+        "bundle_path": bundle_path,
+        "net_id": net_id,
+        "inference_file_name": inference_file_name,
+        "config_file": config_file,
+        "use_onnx": use_onnx,
+        "use_trace": use_trace,
+    }
+    return export_context
+
+
 def verify_all_tensorrt_bundles(download_path="download"):
     """
     This function is used to verify all bundles that support TensorRT.
 
     """
+
     for bundle in include_verify_tensorrt_list:
-        print(f"start verifying bundle {bundle}.")
-        download_latest_bundle(bundle_name=bundle, models_path="models", download_path=download_path)
-        bundle_path = os.path.join(download_path, bundle)
-        net_id, inference_file_name = "network_def", _find_bundle_file(
-            os.path.join(bundle_path, "configs"), "inference"
-        )
-        config_file = os.path.join("configs", inference_file_name)
+        print(f"start verifying bundle {bundle} into TensorRT module.")
+        export_context = get_export_required_files(bundle, download_path)
         for precision in ["fp32", "fp16"]:
+            export_context["precision"] = precision
             try:
-                verify_tensorrt(bundle_path=bundle_path, net_id=net_id, config_file=config_file, precision=precision)
-                print(f"export bundle {bundle} weights into TensorRT module with precision {precision} successfully.")
+                verify_tensorrt(export_context)
+                print(f"export bundle {bundle} with precision {precision} successfully.")
             except BaseException:
-                print(f"verify bundle {bundle} with precision {precision} failed.")
+                print(f"export bundle {bundle} with precision {precision} failed.")
                 raise
     print("all TensorRT supported bundles are verified correctly.")
 
 
+def verify_all_onnx_tensorrt_bundles(download_path="download"):
+    """
+    This function is used to verify all bundles that support ONNX-TensorRT.
+
+    """
+
+    for bundle in include_verify_onnx_tensorrt_list:
+        print(f"start verifying export bundle {bundle} into ONNX-TensorRT module.")
+        export_context = get_export_required_files(bundle, download_path, use_onnx=True, use_trace=True)
+        for precision in ["fp32", "fp16"]:
+            export_context["precision"] = precision
+            try:
+                verify_tensorrt(export_context)
+                print(f"export bundle {bundle} with precision {precision} successfully.")
+            except BaseException:
+                print(f"export bundle {bundle} with precision {precision} failed.")
+                raise
+    print("all ONNX-TensorRT supported bundles are verified correctly.")
+
+
 if __name__ == "__main__":
     verify_all_tensorrt_bundles()
+    verify_all_onnx_tensorrt_bundles()
