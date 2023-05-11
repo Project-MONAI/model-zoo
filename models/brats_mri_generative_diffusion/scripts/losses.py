daf6b674d1a08ad2dee1795fcb24bda0ec899a7e
Can-Zhao <volcanofly@gmail.com>
2023-05-11 12:38:01 -0700

    reformat
    Signed-off-by: Can-Zhao <volcanofly@gmail.com>

--- models/brats_mri_generative_diffusion/scripts/losses.py
+++ models/brats_mri_generative_diffusion/scripts/losses.py
@@ -21,9 +21,11 @@ perceptual_weight = 0.1
 #     If too small, latent space will not be regularized enough for the diffusion model
 kl_weight = 1e-7
 
+
 def KL_loss(z_mu, z_sigma):
     kl_loss = 0.5 * torch.sum(z_mu.pow(2) + z_sigma.pow(2) - torch.log(z_sigma.pow(2)) - 1, dim=[1, 2, 3, 4])
     return torch.sum(kl_loss) / kl_loss.shape[0]
+
 
 def generator_loss(gen_images, real_images, z_mu, z_sigma, disc_net, loss_perceptual):
     recons_loss = intensity_loss(gen_images, real_images)
