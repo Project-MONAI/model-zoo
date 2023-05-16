e80743d234aff5b20219febded6b34a348daa6a6
Can-Zhao <volcanofly@gmail.com>
2023-05-15 20:00:49 -0700

    add install file for 2d ldm
    Signed-off-by: Can-Zhao <volcanofly@gmail.com>

---
+++ ci/install_scripts/install_brats_mri_axial_slices_generative_diffusion.sh
@@ -0,0 +1,14 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# install cython to avoid error:
+# https://github.com/Project-MONAI/model-zoo/actions/runs/4980530305/jobs/8913560199#step:2:550
+pip install git+https://github.com/Project-MONAI/GenerativeModels.git@0.2.1
