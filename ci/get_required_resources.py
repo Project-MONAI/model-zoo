631ce639bdfbfbad59a859375bf87fec796a68ff
Yiheng Wang <vennw@nvidia.com>
2023-05-31 13:54:08 +0800

    add script to determine resources
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- ci/get_required_resources.py
+++ ci/get_required_resources.py
@@ -11,22 +11,27 @@
 
 
 import argparse
+import os
 
 from utils import get_changed_bundle_list
 
 
-def get_changed_bundle(changed_dirs):
+def get_required_resources(changed_dirs):
     """
-    This function is used to get all changed bundles, a string which
-    contains all bundle names will be printed, and can be used in shell scripts.
+    This function is used to determine if GPU or multi-GPU resources are needed.
+    A string which contains two flags will be printed, and can be used in shell scripts.
     """
-    bundle_names = ""
-    root_path = "models"
+    gpu_flag, mgpu_flag = False, False
+    root_path, unit_test_path = "models", "ci/unit_tests"
     bundle_list = get_changed_bundle_list(changed_dirs, root_path=root_path)
-
+    if len(bundle_list) == 0:
+        print(f"{gpu_flag} {mgpu_flag}")
+    gpu_flag = True
     for bundle in bundle_list:
-        bundle_names += f"{bundle} "
-    print(bundle_names)
+        mgpu_test_file = os.path.join(unit_test_path, f"test_{bundle}_dist.py")
+        if os.path.exists(mgpu_test_file):
+            mgpu_flag = True
+    print(f"{gpu_flag} {mgpu_flag}")
 
 
 if __name__ == "__main__":
@@ -34,4 +39,4 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--f", type=str, help="changed files.")
     args = parser.parse_args()
     changed_dirs = args.f.splitlines()
-    get_changed_bundle(changed_dirs)
+    get_required_resources(changed_dirs)
