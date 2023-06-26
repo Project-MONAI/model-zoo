8683e4290b51e22f0a4fc59560b90ef9436478c6
Yiheng Wang <68361391+yiheng-wang-nv@users.noreply.github.com>
2023-06-26 15:44:00 +0800

    Add more unit tests (#455)
    Fixes # .
    
    ### Description
    This PR is used to add unit tests on the following bundles:
    
    - [x] spleen_deepedit_annotation
    - [x] pancreas_ct_dints_segmentation
    - [x] swin_unetr_btcv_segmentation
    - [x] pathology_tumor_detection
    
    ### Status
    **Ready**
    
    ### Please ensure all the checkboxes:
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Codeformat tests passed locally by running `./runtests.sh
    --codeformat`.
    - [ ] In-line docstrings updated.
    - [ ] Update `version` and `changelog` in `metadata.json` if changing an
    existing bundle.
    - [ ] Please ensure the naming rules in config files meet our
    requirements (please refer to: `CONTRIBUTING.md`).
    - [ ] Ensure versions of packages such as `monai`, `pytorch` and `numpy`
    are correct in `metadata.json`.
    - [ ] Descriptions should be consistent with the content, such as
    `eval_metrics` of the provided weights and TorchScript modules.
    - [ ] Files larger than 25MB are excluded and replaced by providing
    download links in `large_file.yml`.
    - [ ] Avoid using path that contains personal information within config
    files (such as use `/home/your_name/` for `"bundle_root"`).
    
    ---------
    
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

---
+++ ci/unit_tests/test_swin_unetr_btcv_segmentation_dist.py
@@ -0,0 +1,80 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shutil
+import tempfile
+import unittest
+
+import numpy as np
+import torch
+from monai.data import ITKWriter
+from parameterized import parameterized
+from utils import export_config_and_run_mgpu_cmd
+
+TEST_CASE_1 = [
+    {
+        "bundle_root": "models/swin_unetr_btcv_segmentation",
+        "images": "$list(sorted(glob.glob(@dataset_dir + '/image_*.nii.gz')))",
+        "labels": "$list(sorted(glob.glob(@dataset_dir + '/label_*.nii.gz')))",
+        "val_interval": 1,
+        "train#random_transforms#0#num_samples": 1,
+        "train#deterministic_transforms#3#pixdim": [1.0, 1.0, 1.0],
+        "train#trainer#max_epochs": 1,
+        "train#dataset#cache_rate": 0.0,
+        "train#dataloader#batch_size": 1,
+        "validate#dataset#cache_rate": 0.0,
+    }
+]
+
+
+class TestSwinUnetrMGPU(unittest.TestCase):
+    def setUp(self):
+        self.dataset_dir = tempfile.mkdtemp()
+        dataset_size = 12
+        input_shape = (96, 96, 96)
+        writer = ITKWriter(output_dtype=np.uint8)
+        for s in range(dataset_size):
+            test_image = np.random.randint(low=0, high=2, size=input_shape).astype(np.int8)
+            test_label = np.random.randint(low=0, high=14, size=input_shape).astype(np.int8)
+            image_filename = os.path.join(self.dataset_dir, f"image_{s}.nii.gz")
+            label_filename = os.path.join(self.dataset_dir, f"label_{s}.nii.gz")
+            writer.set_data_array(test_image, channel_dim=None)
+            writer.set_metadata({"affine": np.eye(4), "original_affine": -1 * np.eye(4)})
+            writer.write(image_filename)
+            writer.set_data_array(test_label, channel_dim=None)
+            writer.set_metadata({"affine": np.eye(4), "original_affine": -1 * np.eye(4)})
+            writer.write(label_filename)
+
+    def tearDown(self):
+        shutil.rmtree(self.dataset_dir)
+
+    @parameterized.expand([TEST_CASE_1])
+    def test_train_mgpu_config(self, override):
+        override["dataset_dir"] = self.dataset_dir
+        bundle_root = override["bundle_root"]
+        train_file = os.path.join(bundle_root, "configs/train.json")
+        mgpu_train_file = os.path.join(bundle_root, "configs/multi_gpu_train.json")
+        output_path = os.path.join(bundle_root, "configs/train_override.json")
+        n_gpu = torch.cuda.device_count()
+        export_config_and_run_mgpu_cmd(
+            config_file=[train_file, mgpu_train_file],
+            logging_file=os.path.join(bundle_root, "configs/logging.conf"),
+            meta_file=os.path.join(bundle_root, "configs/metadata.json"),
+            override_dict=override,
+            output_path=output_path,
+            ngpu=n_gpu,
+            check_config=True,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
