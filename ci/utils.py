d9080553c1f35e93d4da1ec8af7b110f96953090
Yiheng Wang <vennw@nvidia.com>
2023-07-11 12:23:46 +0800

    add --global
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- ci/utils.py
+++ ci/utils.py
@@ -127,10 +127,10 @@ def push_new_model_info_branch(model_info_path: str):
     branch_name = "auto-update-model-info"
     create_push_cmd = f"git checkout -b {branch_name}; git push --set-upstream origin {branch_name}"
 
-    git_config = f"git config user.email {email}; git config user.name {username}"
+    git_config = f"git config --global user.email {email}; git config --global user.name {username}"
     commit_message = "git commit -m 'auto update model_info'"
     full_cmd = f"{git_config}; git add {model_info_path}; {commit_message}; {create_push_cmd}"
-
+    print(full_cmd)
     call_status = subprocess.run(full_cmd, shell=True)
     call_status.check_returncode()
 
