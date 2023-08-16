b05ff976ae2328466ce1a520672eaaf3ac145b35
Yiheng Wang <vennw@nvidia.com>
2023-08-16 19:25:15 +0800

    update import method for github
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- ci/utils.py
+++ ci/utils.py
@@ -18,10 +18,11 @@ import shutil
 import subprocess
 from typing import List
 
-from github import Github
 from monai.apps.utils import download_url
 from monai.bundle.config_parser import ConfigParser
-from monai.utils import look_up_option
+from monai.utils import look_up_option, optional_import
+
+Github, _ = optional_import("github", name="Github")
 
 SUPPORTED_HASH_TYPES = {"md5": hashlib.md5, "sha1": hashlib.sha1, "sha256": hashlib.sha256, "sha512": hashlib.sha512}
 
