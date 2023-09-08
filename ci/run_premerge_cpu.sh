d7ae2b4123523a92fc308c95eccb1b46d0794a1d
Yiheng Wang <68361391+yiheng-wang-nv@users.noreply.github.com>
2023-09-08 16:39:08 +0800

    500 Fix deepedit orientation issue [skip mgpu] (#501)
    Fixes #500 .
    
    ### Description
    This PR is used to fix the orientation issue of the spleen deepedit
    annotation bundle.
    This PR refers to https://github.com/Project-MONAI/MONAILabel/pull/1537
    
    
    ### Status
    **Ready**
    
    ### Please ensure all the checkboxes:
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Codeformat tests passed locally by running `./runtests.sh
    --codeformat`.
    - [ ] In-line docstrings updated.
    - [ ] Update `version` and `changelog` in `metadata.json` if changing an
    existing bundle.
    - [ ] Please ensure the naming rules in config files meet our
    requirements (please refer to: `CONTRIBUTING.md`).
    - [ ] Ensure versions of packages such as `monai`, `pytorch` and `numpy`
    are correct in `metadata.json`.
    - [ ] Descriptions should be consistent with the content, such as
    `eval_metrics` of the provided weights and TorchScript modules.
    - [ ] Files larger than 25MB are excluded and replaced by providing
    download links in `large_file.yml`.
    - [ ] Avoid using path that contains personal information within config
    files (such as use `/home/your_name/` for `"bundle_root"`).
    
    ---------
    
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- ci/run_premerge_cpu.sh
+++ ci/run_premerge_cpu.sh
@@ -29,23 +29,9 @@ elif [[ $# -gt 1 ]]; then
     exit 1
 fi
 
-init_pipenv() {
-    echo "initializing pip environment: $1"
-    pipenv install --python=3.8 -r $1
-    export PYTHONPATH=$PWD
-}
-
-remove_pipenv() {
-    echo "removing pip environment"
-    pipenv --rm
-    rm Pipfile Pipfile.lock
-    pipenv --clear
-    df -h
-}
-
 verify_bundle() {
     echo 'Run verify bundle...'
-    init_pipenv requirements.txt
+    pip install -r requirements.txt
     head_ref=$(git rev-parse HEAD)
     git fetch origin dev $head_ref
     # achieve all changed files in 'models'
@@ -53,30 +39,28 @@ verify_bundle() {
     if [ ! -z "$changes" ]
     then
         # get all changed bundles
-        bundle_list=$(pipenv run python $(pwd)/ci/get_changed_bundle.py --f "$changes")
+        bundle_list=$(python $(pwd)/ci/get_changed_bundle.py --f "$changes")
         if [ ! -z "$bundle_list" ]
         then
-            pipenv run python $(pwd)/ci/prepare_schema.py --l "$bundle_list"
+            python $(pwd)/ci/prepare_schema.py --l "$bundle_list"
             echo $bundle_list
             for bundle in $bundle_list;
             do
-                init_pipenv requirements-dev.txt
+                pip install -r requirements-dev.txt
                 # get required libraries according to the bundle's metadata file
-                requirements=$(pipenv run python $(pwd)/ci/get_bundle_requirements.py --b "$bundle")
+                requirements=$(python $(pwd)/ci/get_bundle_requirements.py --b "$bundle")
                 if [ ! -z "$requirements" ]; then
                     echo "install required libraries for bundle: $bundle"
-                    pipenv install -r "$requirements"
+                    pip install -r "$requirements"
                 fi
                 # verify bundle
-                pipenv run python $(pwd)/ci/verify_bundle.py -b "$bundle" -m "min"  # min tests on cpu
-                remove_pipenv
+                python $(pwd)/ci/verify_bundle.py -b "$bundle" -m "min"  # min tests on cpu
             done
         else
             echo "this pull request does not change any bundles, skip verify."
         fi
     else
         echo "this pull request does not change any files in 'models', skip verify."
-        remove_pipenv
     fi
 }
 
