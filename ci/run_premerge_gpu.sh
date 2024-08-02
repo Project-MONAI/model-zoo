f74a80440a36892c5b9fc6e36d5aad3ed800af79
Yiheng Wang <vennw@nvidia.com>
2024-08-02 15:15:26 +0800

    avoid duplicate unzip
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- ci/run_premerge_gpu.sh
+++ ci/run_premerge_gpu.sh
@@ -82,10 +82,15 @@ verify_bundle() {
                 # unzip data for tests
                 DATA_DIR="$(pwd)/models/maisi_ct_generative/datasets"
                 ZIP_FILE="$DATA_DIR/all_masks_flexible_size_and_spacing_3000.zip"
+                UNZIP_DIR="$DATA_DIR/all_masks_flexible_size_and_spacing_3000"
                 if [ -f "$ZIP_FILE" ]; then
-                    echo "Unzipping files for MAISI Bundle..."
-                    unzip $ZIP_FILE -d $DATA_DIR
-                    echo "Unzipping complete."
+                    if [ ! -d "$UNZIP_DIR" ]; then
+                        echo "Unzipping files for MAISI Bundle..."
+                        unzip $ZIP_FILE -d $DATA_DIR
+                        echo "Unzipping complete."
+                    else
+                        echo "Unzipped content already exists, continuing..."
+                    fi
                 fi
             else
                 init_venv requirements-dev.txt
