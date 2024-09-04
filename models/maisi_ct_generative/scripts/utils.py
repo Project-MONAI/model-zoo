b486c32be50f7e0e63864e9ede094b67a1264cbb
Yiheng Wang <vennw@nvidia.com>
2024-09-04 11:27:58 +0800

    remove kl loss
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- models/maisi_ct_generative/scripts/utils.py
+++ models/maisi_ct_generative/scripts/utils.py
@@ -652,28 +652,6 @@ class MapLabelValue:
         return out
 
 
-def kl_loss(z_mu, z_sigma):
-    """
-    Compute the Kullback-Leibler (KL) divergence loss for a variational autoencoder (VAE).
-
-    The KL divergence measures how one probability distribution diverges from a second, expected probability distribution.
-    In the context of VAEs,
-    this loss term ensures that the learned latent space distribution is close to a standard normal distribution.
-
-    Args:
-        z_mu (torch.Tensor): Mean of the latent variable distribution, shape [N,C,H,W,D] or [N,C,H,W].
-        z_sigma (torch.Tensor): Standard deviation of the latent variable distribution, same shape as 'z_mu'.
-
-    Returns:
-        torch.Tensor: The computed KL divergence loss, averaged over the batch.
-    """
-    eps = 1e-10
-    loss = 0.5 * torch.sum(
-        z_mu.pow(2) + z_sigma.pow(2) - torch.log(z_sigma.pow(2) + eps) - 1, dim=list(range(1, len(z_sigma.shape)))
-    )
-    return torch.sum(loss) / loss.shape[0]
-
-
 def dynamic_infer(inferer, model, images):
     """
     Perform dynamic inference using a model and an inferer, typically a monai SlidingWindowInferer.
