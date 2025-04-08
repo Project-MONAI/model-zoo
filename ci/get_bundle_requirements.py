335b766863be7aa2232afc3263fa30b6be2f1c6a
Yiheng Wang <vennw@nvidia.com>
2025-04-08 19:12:54 +0800

    remove xformer workaround code
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- ci/get_bundle_requirements.py
+++ ci/get_bundle_requirements.py
@@ -81,12 +81,7 @@ def get_requirements(bundle, models_path, requirements_file):
             if package_key in metadata.keys():
                 optional_dict = metadata[package_key]
                 for name, version in optional_dict.items():
-                    if name not in SPECIAL_LIB_LIST:
-                        libs.append(f"{name}=={version}")
-                    else:
-                        if "pytorch_version" in metadata.keys():
-                            # remove torch from libs
-                            libs = [lib for lib in libs if "torch" not in lib or lib == "pytorch-ignite"]
+                    libs.append(f"{name}=={version}")
 
         if len(libs) > 0:
             with open(requirements_file, "w") as f:
