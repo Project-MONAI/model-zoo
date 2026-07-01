18a22ce20b12f979e7df8097f33866eeac5010d4
hartnettlabteam <hartnettlab.team@gmail.com>
2026-07-01 13:02:56 -0700

    Update models/oir_flatmount_segmentation/model_transformer.py
    Co-authored-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    Signed-off-by: hartnettlabteam <hartnettlab.team@gmail.com>

--- models/oir_flatmount_segmentation/model_transformer.py
+++ models/oir_flatmount_segmentation/model_transformer.py
@@ -2,8 +2,8 @@ import timm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from model import AttentionGate, ConvBNReLU, scSE
+
 
 class TransformerEncoder(nn.Module):
 	def __init__(self, backbone: str = "swin_tiny_patch4_window7_224", in_ch: int = 1, pretrained: bool = True):
