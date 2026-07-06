7d67e8977dcc7d4422563f52f8c910fcebb5ff4f
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2026-07-05 21:15:46 -0400

    Better thought out
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>

--- ci/get_bundle_requirements.py
+++ ci/get_bundle_requirements.py
@@ -77,19 +77,22 @@ def get_requirements(bundle, models_path, requirements_file):
         if "numpy_version" in metadata.keys():
             numpy_version = metadata["numpy_version"]
             libs.append(f"numpy=={numpy_version}")
+
         for package_key in ["optional_packages_version", "required_packages_version"]:
-            if package_key in metadata.keys():
-                optional_dict = metadata[package_key]
-                for name, version in optional_dict.items():
-                    if name in special_dependencies_list:
-                        continue
+            for name, version in metadata.get(package_key, {}).items():
+                if name in special_dependencies_list:
+                    continue
 
-                    version = version.strip()
-                    version_line = f"{name}=={version}"
-                    if version[0] in {"<", ">", "="}:  # operator included in version, don't add ==
-                        version_line = f"{name}{version}"
+                version = version.strip()
 
-                    libs.append(version_line)
+                if not version:  # blank version, just add name without version specification
+                    version_line = str(name)
+                elif version[0] in {"<", ">", "=", "!", "~"}:  # operator included in version, don't add ==
+                    version_line = f"{name}{version}"
+                else:
+                    version_line = f"{name}=={version}"  # assume exact version specification
+
+                libs.append(version_line)
 
         if len(libs) > 0:
             with open(requirements_file, "w") as f:
