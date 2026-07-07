3c95a43e11342a8a9320f2e4f18ef7479004b4f1
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2026-07-06 22:23:23 -0400

    Fix
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>

--- ci/utils_deparate_ngc.py
+++ ci/utils_deparate_ngc.py
@@ -24,8 +24,6 @@ from monai.utils import look_up_option, optional_import
 
 Github, _ = optional_import("github", name="Github")
 
-gdown, has_gdown = optional_import("gdown")
-
 SUPPORTED_HASH_TYPES = {"md5": hashlib.md5, "sha1": hashlib.sha1, "sha256": hashlib.sha256, "sha512": hashlib.sha512}
 
 
